8bf704989164ac23c166000f29c0cb842c6cd3e4
Neil Hansen <neil.hansen.31@gmail.com>
2021-09-23 17:34:08 -0700

    Add build

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+yarn run esbuild \
+    --bundle \
+    --outdir=public/dist \
+    --loader:.png=file \
+    --loader:.woff2=file \
+    --loader:.svg=file \
+    --public-path=http://localhost:8000/dist \
+    --format=iife \
+    --define:process.env.NODE_ENV="\"production\"" \
+    src/index.tsx
